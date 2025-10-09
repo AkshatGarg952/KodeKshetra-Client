b427aea9ed69dabb7b116349e1d5cab73c766129
akshat45 <145849325+AkshatGarg952@users.noreply.github.com>
2025-10-09 21:37:45 +0530

    Update LoadingSpinner.js

--- src/components/Landpage/LoadingSpinner.js
+++ src/components/Landpage/LoadingSpinner.js
@@ -4,9 +4,9 @@ const LoadingSpinner = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="relative w-8 h-8">
-        <div className="absolute w-full h-full border-4 border-transparent border-t-cyber-cyan border-r-electric-blue rounded-full animate-spin shadow-[0_0_15px_var(--cyber-cyan)]"></div>
+        <div className="absolute w-full h-full border-4 border-transparent border-t-cyan-400 border-r-blue-400 rounded-full animate-spin shadow-[0_0_15px_rgba(0,191,255,0.5)]"></div>
         <div 
-          className="absolute w-6 h-6 top-1 left-1 border-4 border-transparent border-t-electric-blue border-l-cyber-cyan rounded-full animate-spin shadow-[0_0_10px_var(--electric-blue)]" 
+          className="absolute w-6 h-6 top-1 left-1 border-4 border-transparent border-t-blue-400 border-l-cyan-400 rounded-full animate-spin shadow-[0_0_10px_rgba(0,150,255,0.5)]" 
           style={{ animationDirection: 'reverse', animationDuration: '0.6s' }}
         ></div>
       </div>
